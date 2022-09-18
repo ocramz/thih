6c34e1ba880c0a662c00d4a1350d36acb8262112
Marco Zocca <marco.zocca@unfoldml.com>
2022-09-18 16:31:18 +0200

    builds with ghc 9.0.2

---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------
+-- Test Framework:
+import qualified SourcePrelude as SP (main)
+import Testbed
+import HaskellPrims
+
+
+main :: IO ()
+main = SP.main
